2be426598e781a61c7319838612d97a044f72dc5
Yash Jha <yashkumar11042004@gmail.com>
2025-12-31 21:48:28 +0530

    Restore Pages folder

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Poppular from '../Components/Poppular/Poppular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Footer from '../Components/Footer/Footer'
+function Shop() {
+  return (
+    <>
+      <Hero></Hero>
+      <Poppular></Poppular>
+      <Offers></Offers>
+      <NewCollections></NewCollections>
+      <NewsLetter></NewsLetter>
+      
+      </>
+  )
+}
+
+export default Shop
