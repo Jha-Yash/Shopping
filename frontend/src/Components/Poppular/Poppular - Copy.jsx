b6820ed348ee164fd9eeb8262041176f08330a9c
Yash Jha <yashkumar11042004@gmail.com>
2025-12-31 21:42:41 +0530

    Restore Components folder

---
+++ frontend/src/Components/Poppular/Poppular - Copy.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Poppular.css'
+import data_product from '../Assets/data'
+import Item from '../Item/Item'
+
+const Poppular = () => {
+  return (
+    <div className='poppular'>
+      <h1>POPPULAR IN WOMEN</h1>
+      <hr />
+      <div className='poppular-item'>
+        {data_product.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Poppular
