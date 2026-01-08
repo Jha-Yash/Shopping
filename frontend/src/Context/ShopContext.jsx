19858f26474b82c753ffe015202cb680ef1b8241
Yash Jha <yashkumar11042004@gmail.com>
2026-01-08 22:43:34 +0530

    cart fn add

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,12 +1,35 @@
-import React, { createContext } from "react";// React → for JSX createContext → to create a global storage
+import React, { createContext ,useState } from "react";// React → for JSX createContext → to create a global storage,useState → 
+// to manage cart state
 import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);//Create a global box called ShopContext which can store data for the whole app”
 // Right now it’s empty (null).
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;//for every product card is empty initially
+  }
+  return cart;
+};
+
 const ShopContextProvider = (props) => {//This is a wrapper component Everything inside it will get access to global data
 
-  const contextValue = { all_product };//These are the things I want to share globally
+  const [cartItems, setCartItems] = useState(getDefaultCart());// Global cart state (cart is empty initially)
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+   const contextValue = {
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };//These are the things(data and fns) I want to share globally through context
 
   return (
     <ShopContext.Provider value={contextValue}>
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import BreadCrums from "../Components/BreadCrums/BreadCrums";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);//get data from global context //ShopContext → where all products are stored
@@ -16,6 +18,8 @@ const Product = () => {
     <div>
       <BreadCrums product={product} /> {/*Breadcrumb → component that shows navigation path*/}
        <ProductDisplay product={product} />
+       <DescriptionBox></DescriptionBox>
+       <RelatedProducts></RelatedProducts>
     </div>
   );
 };
